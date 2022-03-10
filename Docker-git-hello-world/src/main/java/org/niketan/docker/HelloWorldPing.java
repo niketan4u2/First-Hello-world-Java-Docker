97b286274cd66c288bea1d76c62f2682d3de6b6b
niketan4u2 <47866818+niketan4u2@users.noreply.github.com>
2022-03-10 10:55:16 +0530

    First Commit

---
+++ Docker-git-hello-world/src/main/java/org/niketan/docker/HelloWorldPing.java
@@ -0,0 +1,13 @@
+package org.niketan.docker;
+
+
+public class HelloWorldPing {
+	
+	public static void main(String args[]) throws Exception{
+		for(int i = 0 ; i < 100; i++){
+			System.out.println("Hello World Ping " + i );
+			Thread.sleep(1000);
+		}
+	}
+	
+}
